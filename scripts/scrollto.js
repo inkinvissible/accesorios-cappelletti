9248843e91af28b77025fe6348051cdafa07e99a
inkinvissible <agusmartin113@gmail.com>
2022-02-08 11:59:41 -0300

    First Commit

---
+++ scripts/scrollto.js
@@ -0,0 +1,17 @@
+//Scroll To
+const links = document.querySelectorAll(".scrollTo");
+
+for (const link of links) {
+  link.addEventListener("click", clickHandler);
+}
+
+function clickHandler(e) {
+  e.preventDefault();
+  const href = this.getAttribute("href");
+  const offsetTop = document.querySelector(href).offsetTop;
+
+  scroll({
+    top: offsetTop,
+    behavior: "smooth"
+  });
+}
