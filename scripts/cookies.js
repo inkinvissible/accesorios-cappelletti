c1446213d878dabde9ee0b46ebc0e9fc84a4e9fb
inkinvissible <agusmartin113@gmail.com>
2022-02-10 10:35:38 -0300

    Cambios menores

---
+++ scripts/cookies.js
@@ -0,0 +1,6 @@
+// $("#crossOverlay").click = () =>{
+// $("#cookiesOverlay").css("min-height", "0px");
+// };
+$("#crossOverlay").click(function () {
+    $("#cookiesOverlay").css("min-height", "0px");
+});
